f40b76088a9af5cc456fccfe675a0669fe09c724
Rojsasha <ar@trinitydigital.ru>
2020-12-23 17:49:40 +0300

    add user request

---
+++ app/src/main/java/ru/trinitydigital/cameraimage/SampleDataClass.java
@@ -0,0 +1,39 @@
+package ru.trinitydigital.cameraimage;
+
+public class SampleDataClass {
+
+    private String id;
+    private int number;
+    private long longest;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public long getLongest() {
+        return longest;
+    }
+
+    public void setLongest(long longest) {
+        this.longest = longest;
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/ru/trinitydigital/cameraimage/SampleDataClassKotlin.kt
@@ -0,0 +1,7 @@
+package ru.trinitydigital.cameraimage
+
+data class SampleDataClassKotlin(
+    val id: String? = null,
+    val number: Int = 0,
+    val longest: Long = 0
+)
